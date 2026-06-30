385df1ac1197ca90bb4c8d92cbaaffd72588b25d
scg1004-lab <scg1004@gmail.com>
2026-06-30 10:00:58 +0200

    Add files via upload

---
+++ ratings.js
@@ -0,0 +1,19 @@
+// Valoraciones iniciales predeterminadas para las cartas
+const initialRatings = {
+  "men_neuromarketing_precios.html": 5,
+  "men_triangulo_maridaje.html": 5,
+  "men_premium_street_food.html": 5,
+  "men_storytelling_chef.html": 5,
+  "men_botanico_editorial.html": 5,
+  "men_minimalista_y_moderno.html": 4,
+  "men_r_stico_y_ecol_gico.html": 4,
+  "men_editorial_premium.html": 4,
+  "men_fluido_terracota.html": 4,
+  "men_galer_a_gastron_mica.html": 3,
+  "men_creativo_nocturno.html": 3,
+  "men_fino_rock_and_roll.html": 2,
+  "men_c_mic_neo_brutalista.html": 2,
+  "men_vanguardia_y_premios.html": 2,
+  "Carta La Huella Vegana.pdf": 0,
+  "CARTA HUELLA 2026-3.pdf": 0
+};
